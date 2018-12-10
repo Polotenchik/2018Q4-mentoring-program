3de584e89a7dc44bfa8ce081fe18df4adcfec61a
Polotenchik Koala <Polotenchik.koala@gmail.com>
2018-12-10 22:01:15 +0300

    feat: add header component

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 import Spinner from './components/spinner';
+import Header from './components/header';
 import ChannelList from './components/channelList';
 
 class App {
     constructor() {
         this.spinner = new Spinner();
+        this.header = new Header();
         this.channelListlist = new ChannelList();
     };
 
---
+++ js/components/header.js
@@ -0,0 +1,8 @@
+export default class Header {
+    render() {
+        const header = document.querySelector('.header');
+        const h1 = document.createElement('h1');
+        const small = document.createElement('small');
+        const btnBlock this.createBtnBlock
+    }
+}
