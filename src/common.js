17282f24be4ff4971c49b609843bf460db2996df
Polotenchik Koala <Polotenchik.koala@gmail.com>
2019-01-14 21:38:20 +0300

    docs:update files

--- src/common.js
+++
@@ -1,9 +0,0 @@
-export const promiseDelay = (time, data) => new Promise(res => setTimeout(() => res(data), time));
-
-export const showError = (err) => {
-    const errorBlock = document.querySelector('.error-block');
-    const p = document.createElement('p');
-    p.textContent = `Error ${err.messaage}. Try to reload page.`;
-    errorBlock.appendChild(p);
-    errorBlock.classList.add('active');
-};
